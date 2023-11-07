7fe0263acd28c4d6c9f99a72385b88d54db60556
rodolfodom <rodolfodom7@gmail.com>
2023-11-06 22:27:14 -0600

    fix/user signup end point sends now a confirmation link by email

--- recuerdosBackend/controllers/user/signUpController.js
+++ recuerdosBackend/controllers/user/signUpController.js
@@ -1,46 +1,72 @@
-import User from "../../models/user.js"
+import User from "../../models/user.js";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
+import nodemailer from "nodemailer"
+
+dotenv.config();
 const rounds = 5;
-const passwordRegExp = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!.]).{8,}$/
+const passwordRegExp =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@#$%^&+=!.]).{8,}$/;
 
-export default async function signUpController(req, res){
-    try {
-        if(!passwordRegExp.test(req.body.password)){
-            throw new Error("La contraseña no cumple los requisitos", {cause: "password"})
-        }
-        const newUserData = {
-            ...req.body,
-            roleID: 2,
-            password: await bcrypt.hash(req.body.password, rounds)
-        }
-        const newUser = User.build(newUserData)
-        await newUser.save()
+export default async function signUpController(req, res) {
+  try {
+    if (!passwordRegExp.test(req.body.password)) {
+      throw new Error("La contraseña no cumple los requisitos", {
+        cause: "password",
+      });
+    }
+    const newUserData = {
+      ...req.body,
+      roleID: 2,
+      password: await bcrypt.hash(req.body.password, rounds),
+    };
+    const newUser = User.build(newUserData);
+    await newUser.save();
 
-        const jsonResponse = {
-            success: true,
-            data: {
-                msg: "Usuario registrado exitosamente",
-                user: newUser.get("email")
-            }
-        }
+    const jsonResponse = {
+      success: true,
+      data: {
+        msg: "Usuario registrado exitosamente",
+        user: newUser.get("email"),
+      },
+    };
 
-        res.status(200).json(jsonResponse)
+    const confirmationLink = `${process.env.ORIGIN}/user/confirmation/${newUser.get('token')}`;
+    var transport = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      port: process.env.MAIL_PORT,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
 
-    } catch (error) {
-        console.log("***********************************************")
-        console.log(error.cause)
-        console.log("***********************************************")
+    const mailOptions = {
+      from: process.env.MAIL_SENDER,
+      to: newUser.get("email"),
+      subject: "Confirmación de correo electrónico",
+      html: `<b>Por favor, haz clic en el <a href=${confirmationLink}>enlace de confirmación</a> para activar tu cuenta.</b>`,
+    };
+
+    const mail = await transport.sendMail(mailOptions)
 
 
 
-        const jsonResponse = {
-            success: false,
-            data: {
-                msg: error.message,
-                cause: error.cause? [error.cause]: error.errors.map(error => error.path)
-            }
-        }
+    res.status(200).json(jsonResponse);
+  } catch (error) {
 
-        res.status(400).json(jsonResponse)
-    } 
+    console.log(error)
+
+    const jsonResponse = {
+      success: false,
+      data: {
+        msg: error.message,
+        cause: error.cause
+          ? [error.cause]
+          : error.errors.map((error) => error.path),
+      },
+    };
+
+    res.status(400).json(jsonResponse);
+  }
 }
